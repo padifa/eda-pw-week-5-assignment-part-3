9b9722fc8e758b339c818a99fdacb734c4e7808e
padifa <padifa7@live.fr>
2024-08-18 21:10:11 -0500

    created a function finByArtist

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,114 +1,81 @@
-console.log('***** Music Collection *****')
+console.log("***** Music Collection *****");
 // Safe Zone -- Write code below this line
 let myCollection = [];
-//- This function should:
-//- Take in a `collection` parameter. (This allows the function to be reused to add an album to any array of album objects.)
-//- Take in the album's `title`, `artist`, `yearPublished` as parameters.
-//- Create a new object having the above properties.
-//  - *NOTE*: Your object's properties **must** have `title`, `artist`, and `yearPublished` in order for this assignment's automated tests to work correctly!
-//- Add the new object to the end of the `collection` array.
-//- `return` the newly created object.
+
 
 function addToCollection(collection, title, artist, yearPublished) {
-  const albumOne = {
-  title: "someone you loved",
-  artist: "lewis Capaldi",
-  yearPublished:"2019"
+  const album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+};
+collection.push(album);
+return album;
 }
-myCollection.push(albumOne);
+console.log(addToCollection(myCollection, 'someone you loved', 'lewis Capaldi', '2019'));
+console.log(addToCollection(myCollection, 'Billie Jean', 'Michael Jackson', '1983'));
+console.log(addToCollection(myCollection, 'Lonely', 'Akon', '2005'));
+console.log(addToCollection(myCollection, 'One Love', 'Bob Marley', '1965'));
+console.log(addToCollection(myCollection, 'I will always love you', 'Whitney Houston', '1992'));
+console.log(addToCollection(myCollection, 'California Love', '2Pac', '1996'));
+
+console.log(myCollection);
 
 
-const albumTwo = {
-  title: "Billie Jean",
-  artist: "Michael Jackson",
-  yearPublished:"1983"
-}
-myCollection.push(albumTwo);
-
-
-const albumThree = {
-  title: "Lonely",
-  artist: "Akon",
-  yearPublished:"2005"
-}
-myCollection.push(albumThree);
-
-
-
-const albumFour = {
-  title: "One Love",
-  artist: "Bob Marley",
-  yearPublished:"1965"
-}
-myCollection.push(albumFour);
-
-
-const albumFive = {
-  title: "someone you loved",
-  artist: "lewis Capaldi",
-  yearPublished:"2019"
-}
-myCollection.push(albumFive);
-
-
-
-const albumSix = {
-  title: "I will always love you",
-  artist: "Whitney Houston",
-  yearPublished:"1992"
-}
-myCollection.push(albumSix);
-
-const albumSeven = {
-  title: "California Love",
-  artist: "2Pac",
-  yearPublished:"1996"
-}
-myCollection.push(albumSeven);
-return myCollection;
-}
-console.log(' My music collection is', myCollection);
-//- Use and Test the `addToCollection` function:
-  //- Add 6 albums to the `myCollection` array. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
- // - `console.log` each album as added using the function's returned value.
-  //- After all are added, console.log the `myCollection` array.
-
-
-
-  //- Create a function named `showCollection`. This function should:
-  function showCollection () {
-    
+function showCollection(collection) {
+  for (let album of collection) {
+      console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
   }
-  //- Take in a `collection` parameter. (This allows it to be reused to show any array of album objects.)
-  //- Loop through the `collection` and `console.log` each album's information formatted **within a single string**, like: `TITLE by ARTIST, published in YEAR`.
-
-//- Test the `showCollection` function.
-
-//- Add a function named `findByArtist`. This function should:
-  //- Take in a `collection` parameter. Remember, we want to be able to search any collection!
-  //- Take in an `artist` (string) parameter.
-  //- Create an empty array to hold any matching results, if any.
-  //- Loop through the `collection` and add any album objects with a matching artist to the array.
-  //- Return the array with the matching results. (If no results are found, an empty array should be returned.)
-
-//- Test the `findByArtist` function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are returned.
-
-//> When testing your functions, write all tests in the JavaScript file!
+}
+function findByArtist(collection, artist) {
+  let results = [];
+  for (let album of collection) {
+      if (album.artist === artist) {
+          results.push(album);
+      }
+  }
+  return results;
+}
 
 
+console.log(findByArtist(myCollection, '2Pac')); // Should return albums by Artist A
+console.log(findByArtist(myCollection, 'Lewis Capaldi')); // Should return an empty array
 
+function search(collection, searchCriteria) {
+  if (!searchCriteria || (!searchCriteria.artist && !searchCriteria.year)) {
+      return collection;
+  }
 
-// PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
-// as a lil' chunk of friendly code that you don't need to understand right now.
-// (It's used for automated testing.)
+  let results = [];
+  for (let album of collection) {
+      let artistMatch = searchCriteria.artist ? album.artist === searchCriteria.artist : true;
+      let yearMatch = searchCriteria.year ? album.yearPublished === searchCriteria.year : true;
+
+      if (artistMatch && yearMatch) {
+          results.push(album);
+      }
+  }
+  return results;
+}
+
+console.log(search(myCollection, { artist: '2Pac', year: 1996 })); 
+console.log(search(myCollection, { artist: 'lewis capaldi', year: 2019 }));
+console.log(search(myCollection, {})); 
+console.log(search(myCollection)); 
+console.log(search(myCollection, { artist: 'Michael Jackson', year: 1983 })); 
+
 try {
   module.exports = {
-    myCollection: typeof myCollection !== 'undefined' ? myCollection : undefined,
-    addToCollection: typeof addToCollection !== 'undefined' ? addToCollection : undefined,
-    showCollection: typeof showCollection !== 'undefined' ? showCollection : undefined,
-    findByArtist: typeof findByArtist !== 'undefined' ? findByArtist : undefined,
-    search: typeof search !== 'undefined' ? search : undefined,
-  }
+    myCollection:
+      typeof myCollection !== "undefined" ? myCollection : undefined,
+    addToCollection:
+      typeof addToCollection !== "undefined" ? addToCollection : undefined,
+    showCollection:
+      typeof showCollection !== "undefined" ? showCollection : undefined,
+    findByArtist:
+      typeof findByArtist !== "undefined" ? findByArtist : undefined,
+    search: typeof search !== "undefined" ? search : undefined,
+  };
 } catch (e) {
   // Do nothing
 }
