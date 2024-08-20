8adfcb0c403c60fe1a49405a86c59d98bb624729
padifa <padifa7@live.fr>
2024-08-19 20:36:26 -0500

    formatted the code

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,50 +2,70 @@ console.log("***** Music Collection *****");
 // Safe Zone -- Write code below this line
 let myCollection = [];
 
-
 function addToCollection(collection, title, artist, yearPublished) {
   const album = {
     title: title,
     artist: artist,
-    yearPublished: yearPublished
-};
-collection.push(album);
-return album;
+    yearPublished: yearPublished,
+  };
+  collection.push(album);
+  return album;
 }
-console.log(addToCollection(myCollection, 'someone you loved', 'Lewis Capaldi', '2019'));
-console.log(addToCollection(myCollection, 'Billie Jean', 'Michael Jackson', '1983'));
-console.log(addToCollection(myCollection, 'Lonely', 'Akon', '2005'));
-console.log(addToCollection(myCollection, 'One Love', 'Bob Marley', '1965'));
-console.log(addToCollection(myCollection, 'I will always love you', 'Whitney Houston', '1992'));
-console.log(addToCollection(myCollection, 'California Love', '2Pac', '1996'));
+console.log(
+  addToCollection(myCollection, "someone you loved", "Lewis Capaldi", "2019")
+);
+console.log(
+  addToCollection(myCollection, "Billie Jean", "Michael Jackson", "1983")
+);
+console.log(addToCollection(myCollection, "Lonely", "Akon", "2005"));
+console.log(addToCollection(myCollection, "One Love", "Bob Marley", "1965"));
+console.log(
+  addToCollection(
+    myCollection,
+    "I will always love you",
+    "Whitney Houston",
+    "1992"
+  )
+);
+console.log(addToCollection(myCollection, "California Love", "2Pac", "1996"));
 
-console.log(myCollection);
-
+console.log("My music collection is:", myCollection);
 
 function showCollection(collection) {
   for (let album of collection) {
-      console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+    console.log(
+      `${album.title} by ${album.artist}, published in ${album.yearPublished}`
+    );
   }
 }
 showCollection(myCollection);
 
 function findByArtist(collection, artist) {
-  let results = [];
+  let albumFound = [];
   for (let album of collection) {
-      if (album.artist === artist) {
-          results.push(album);
-      }
+    if (album.artist === artist) {
+      albumFound.push(album);
+    }
   }
-  return results;
+  return albumFound;
 }
 
+console.log("The album found is:", findByArtist(myCollection, "2Pac"));
+console.log("The album found is:", findByArtist(myCollection, "Lewis Capaldi"));
+console.log(
+  "The album found is:",
+  findByArtist(myCollection, "Michael Jackson")
+);
+console.log("The album found is:", findByArtist(myCollection, "Bob Marley"));
+console.log(
+  "The album found is:",
+  findByArtist(myCollection, "Whitney Houston")
+);
+console.log("The album found is:", findByArtist(myCollection, "Akon"));
 
-console.log(findByArtist(myCollection, '2Pac')); 
-console.log(findByArtist(myCollection, 'Lewis Capaldi')); 
-console.log(findByArtist(myCollection, 'Michael Jackson'));
-console.log(findByArtist(myCollection, 'Bob Marley'));
-console.log(findByArtist(myCollection, 'Whitney Houston'));
-console.log(findByArtist(myCollection, 'Akon'));
+
+  
+
 try {
   module.exports = {
     myCollection:
